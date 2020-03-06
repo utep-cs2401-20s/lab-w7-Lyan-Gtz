646f4b842b2f13dd09b46b6b2b6fe4dcd4d934d7
Lyan-Gtz <lyan2695@gmail.com>
2020-03-06 14:05:14 -0700

    Finalized method and test cases. Also, Word document included to the project.

--- java/src/SortOfSort.java
+++ java/src/SortOfSort.java
@@ -1,36 +1,37 @@
-class SortOfSort {
-    void sortOfSort(int[] ar){
-        int max = ar[0];
-        int icounter = 0;
-        int jcounter = 0;
-        int maxIndex = 0;
-        int temp = 0;
+public class SortOfSort {
+    public void sortOfSort(int[] ar){
         int i = 0;
         int j = ar.length-1;
+        int icounter = 0;
+        int jcounter = 0;
 
         while (i < j) {
+            int maxIndex = i;
+            int temp = 0;
+            int max = ar[i];
             for (int k = i; k <= j; k++) {
-                if (max < ar[k]) {
+                if (ar[k] > max) {
                     max = ar[k];
                     maxIndex = k;
                 }
-
-                if (jcounter != 2) {
-                    temp = ar[j];
-                    ar[j] = max;
-                    ar[maxIndex] = temp;
-                    jcounter += 1;
-                    j--;
-                } else if (icounter != 2) {
-                    temp = ar[i];
-                    ar[i] = max;
-                    ar[maxIndex] = temp;
-                    icounter += 1;
-                    i++;
-                } else if (icounter == 2 && jcounter == 2) {
-                    icounter = 0;
-                    jcounter = 0;
-                }
+            }
+            if (icounter == 2 && jcounter == 2) {
+                icounter = 0;
+                jcounter = 0;
+            }
+            if (jcounter != 2) {
+                temp = ar[j];
+                ar[j] = max;
+                ar[maxIndex] = temp;
+                j--;
+                jcounter++;
+            }
+            else {
+                temp = ar[i];
+                ar[i] = max;
+                ar[maxIndex] = temp;
+                i++;
+                icounter++;
             }
         }
     }
--- java/src/SortOfSortTestN.java
+++ java/src/SortOfSortTestN.java
@@ -2,11 +2,61 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class SortOfSortTestN {
+public class SortOfSortTestN {
 
     @Test
-    void sortOfSort() {
-        sortOfSort();
+    void sortOfSort1() {
+        SortOfSort sos = new SortOfSort();
+        int[] ar = {4,2};
+        int[] b = new int[] {2,4};
+        sos.sortOfSort(ar);
+        assertArrayEquals(b,ar);
+    /*Test was conducted to see how it will behave when a string of two elements was given. If array was swap it was
+        correct, if not, then the test will fail. Test passed*/
+    }
 
+    @Test
+    void sortOfSort2() {
+        SortOfSort sos = new SortOfSort();
+        int[] ar = {4};
+        int[] b = new int[] {4};
+        sos.sortOfSort(ar);
+        assertArrayEquals(b,ar);
+    /*Test was conducted to know if a single value could be given in the array or if an error was produced since only
+     one value was provided instead of 2. Test passed*/
+    }
+
+    @Test
+    void sortOfSort3() {
+        SortOfSort sos = new SortOfSort();
+        int[] ar = {};
+        int[] b = new int[] {};
+        sos.sortOfSort(ar);
+        assertArrayEquals(b,ar);
+    /*Test was conducted to know if an empty array could be taken and return empty or error. Test passed providing
+    an empty array as result*/
+
+    }
+
+    @Test
+    void sortOfSort4() {
+        SortOfSort sos = new SortOfSort();
+        int[] ar = {9,6,4,1,5};
+        int[] b = new int[] {5,4,1,6,9};
+        sos.sortOfSort(ar);
+        assertArrayEquals(b,ar);
+    /*Test was conducted to know if an array with n number of elements could be sorted as needed or if it could take
+    only certain amount of elements. Test passed and array was sorted correctly*/
+    }
+
+    @Test
+    void sortOfSort5() {
+        SortOfSort sos = new SortOfSort();
+        int[] ar = {9,6,4,1,5,-1,20,-10,7};
+        int[] b = new int[] {7,6,1,-1,-10,4,5,9,20};
+        sos.sortOfSort(ar);
+        assertArrayEquals(b,ar);
+    /*Test was conducted to know if an array with negative and positive values could be taken and sorted as it should be.
+    Test passed and array was sorted as expected*/
     }
 }
